29fd98866e75751d44b1c06b521790b9826ed066
regorxxx <regorxxx@protonmail.com>
2022-12-09 00:07:16 +0100

    Fix typo.

--- helpers/buttons_sbd_menu_config.js
+++ helpers/buttons_sbd_menu_config.js
@@ -616,7 +616,7 @@ function createConfigMenu(parent) {
 			}, flags: !sbd.isCalculatingCache ? MF_STRING : MF_GRAYED});
 			// Tags cache reset Async
 			menu.newEntry({menuName: submenu, entryText: 'Reset tags cache' + (!isFoobarV2 ? '\t-only Fb >= 2.0-' : (sbd.panelProperties.bTagsCache[1] ?  '' : '\t -disabled-')), func: () => {
-				const keys = Object.values(calcTags).filter(t => !t.type.includes('virtual')).map(t => t.tf);
+				const keys = Object.values(tags).filter(t => !t.type.includes('virtual')).map(t => t.tf);
 				const tags = keys.concat([['TITLE'], [globTags.title]])
 					.map((tagName) => {return tagName.map((subTagName) => {return (subTagName.indexOf('$') === -1 ? '%' + subTagName + '%' : subTagName);});})
 					.map((tagName) => {return tagName.join(', ');}).filter(Boolean)
