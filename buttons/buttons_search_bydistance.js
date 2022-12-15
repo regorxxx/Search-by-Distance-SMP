2b2111e33d7e7c5d959b2c455708138641b7d71b
regorxxx <regorxxx@protonmail.com>
2022-12-15 10:50:44 +0100

    - Buttons: generic button ('buttons_search_bydistance.js') can now be configured (although not in the same way than the customizable button). It replaces the buttons for specific methods, since the search method can now be set.

--- buttons/buttons_search_bydistance.js
+++ buttons/buttons_search_bydistance.js
@@ -1,8 +1,9 @@
 ﻿'use strict';
-//05/12/22
+//15/12/22
 
 include('..\\helpers\\buttons_xxx.js');
 include('..\\helpers\\helpers_xxx_properties.js');
+include('..\\helpers\\buttons_xxx_menu.js');
 
 try {window.DefinePanel('Search by Distance Buttons', {author:'xxx'});} catch (e) {/* console.log('Search by Distance Buttons loaded.'); */} //May be loaded along other buttons
 
@@ -14,22 +15,29 @@ prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
 var newButtonsProperties = { //You can simply add new properties here
 };
 newButtonsProperties = {...SearchByDistance_properties, ...newButtonsProperties}; // Add default properties at the beginning to be sure they work 
-delete newButtonsProperties.genreWeight;
-delete newButtonsProperties.styleWeight;
-delete newButtonsProperties.moodWeight;
-delete newButtonsProperties.keyWeight;
-delete newButtonsProperties.dateWeight;
-delete newButtonsProperties.bpmWeight;
-delete newButtonsProperties.dateRange;
-delete newButtonsProperties.bpmRange;
 delete newButtonsProperties.probPick;
+delete newButtonsProperties.minScoreFilter;
 delete newButtonsProperties.scoreFilter;
-delete newButtonsProperties.sbd_max_graph_distance;
-delete newButtonsProperties.dyngenreWeight;
-delete newButtonsProperties.dyngenreRange;
+delete newButtonsProperties.graphDistance;
+delete newButtonsProperties.bSameArtistFilter;
+delete newButtonsProperties.bRandomPick;
+delete newButtonsProperties.bSortRandom;
+delete newButtonsProperties.bProgressiveListOrder;
+delete newButtonsProperties.bUseAntiInfluencesFilter;
+delete newButtonsProperties.bConditionAntiInfluences;
+delete newButtonsProperties.bUseInfluencesFilter;
+delete newButtonsProperties.bSimilArtistsFilter;
+delete newButtonsProperties.poolFilteringTag;
+delete newButtonsProperties.poolFilteringN;
+delete newButtonsProperties.bProgressiveListCreation;
+delete newButtonsProperties.progressiveListCreationN;
+delete newButtonsProperties.bHarmonicMixDoublePass;
+delete newButtonsProperties.bAscii;
+delete newButtonsProperties.bAdvTitle;
 setProperties(newButtonsProperties, prefix, 0); //This sets all the panel properties at once
 {
 	const properties = getPropertiesPairs(newButtonsProperties, prefix, 0);
+	newButtonsProperties = properties;
 	buttonsBar.list.push(properties);
 	// Update cache with user set tags
 	doOnce('Update SBD cache', debounce(updateCache, 3000))({properties});
@@ -44,48 +52,60 @@ setProperties(newButtonsProperties, prefix, 0); //This sets all the panel proper
 */
 
 addButton({
-	'Search by Distance nearest tracks': new themedButton({x: 0, y: 0, w: 106, h: 22}, 'Nearest Tracks', function () {
-		let t0 = Date.now();
-		let t1 = 0;
-		const args = {genreWeight: 15, styleWeight: 10, moodWeight: 5, keyWeight: 10, dateWeight: 25, bpmWeight: 5,  dateRange: 15, 
-					bpmRange: 25, probPick: 100, scoreFilter: 70, sbd_max_graph_distance: music_graph_descriptors.intra_supergenre / 2, 
-					dyngenreWeight: 25, dyngenreRange: 1,
-					properties: getPropertiesPairs(this.buttonsProperties, this.prefix, 0)}; // Mix with only nearest tracks
-		searchByDistance(args); 
-		t1 = Date.now();
-		console.log('Call to searchByDistance NearestTracks took ' + (t1 - t0) + ' milliseconds.');
+	'Search by Distance nearest tracks': new themedButton({x: 0, y: 0, w: 106, h: 22}, 'Nearest Tracks', function (mask) {
+		if (mask === MK_SHIFT) {
+			settingsMenu(this, true, ['buttons_search_bydistance.js'], {bAdvTitle: {popup: globRegExp.title.desc}}).btn_up(this.currX, this.currY + this.currH);
+		} else {
+			// Mix with only nearest tracks
+			const tags = JSON.parse(this.buttonsProperties.tags[1]);
+			tags.genre.weight = 15; tags.style.weight = 10; tags.mood.weight = 5; tags.key.weight = 10; tags.date.weight = 25; tags.bpm.weight = 5;
+			tags.date.range = 15; tags.bpm.range = 25;
+			tags.dynGenre.weight = 25; tags.dynGenre.range = 1;
+			const defArgs = {tags, bRandomPick: true, bHarmonicMixDoublePass: true, properties: this.buttonsProperties}
+			const args = {...defArgs, minScoreFilter: 65, scoreFilter: 70, graphDistance: music_graph_descriptors.intra_supergenre / 2};
+			searchByDistance(args);
+		}
 	}, null, void(0), 'Random mix with only nearest tracks', prefix, newButtonsProperties, chars.wand),
-	'Search by Distance similar tracks': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Tracks', function () {
-		let t0 = Date.now();
-		let t1 = 0;
-		const args = {genreWeight: 15, styleWeight: 10, moodWeight: 5, keyWeight: 5, dateWeight: 25, bpmWeight: 5,  dateRange: 15, 
-					bpmRange: 25, probPick: 100, scoreFilter: 60, sbd_max_graph_distance: music_graph_descriptors.cluster, 
-					dyngenreWeight: 10, dyngenreRange: 1,
-					properties: getPropertiesPairs(this.buttonsProperties, this.prefix, 0)}; // Mix a bit varied on styles/genres most from the same decade
-		searchByDistance(args);
-		t1 = Date.now();
-		console.log('Call to searchByDistance SimilarTracks took ' + (t1 - t0) + ' milliseconds.');
+	'Search by Distance similar tracks': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Tracks', function (mask) {
+		if (mask === MK_SHIFT) {
+			settingsMenu(this, true, ['buttons_search_bydistance.js'], {bAdvTitle: {popup: globRegExp.title.desc}}).btn_up(this.currX, this.currY + this.currH);
+		} else {
+			// Mix a bit varied on styles/genres most from the same decade
+			const tags = JSON.parse(this.buttonsProperties.tags[1]);
+			tags.genre.weight = 15; tags.style.weight = 10; tags.mood.weight = 5; tags.key.weight = 5; tags.date.weight = 25; tags.bpm.weight = 5;
+			tags.date.range = 15; tags.bpm.range = 25;
+			tags.dynGenre.weight = 10; tags.dynGenre.range = 1;
+			const defArgs = {tags, bRandomPick: true, bHarmonicMixDoublePass: true, properties: this.buttonsProperties}
+			const args = {...defArgs, minScoreFilter: 55, scoreFilter: 60, graphDistance: music_graph_descriptors.cluster};
+			searchByDistance(args);
+		}
 	}, null, void(0), 'Random mix a bit varied on styles (but similar genre), most tracks within a decade', prefix, newButtonsProperties, chars.wand),
-	'Search by Distance similar genres': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Genres', function () {
-		let t0 = Date.now();
-		let t1 = 0;
-		const args = {genreWeight: 0, styleWeight: 5, moodWeight: 15, keyWeight: 10, dateWeight: 25, bpmWeight: 5,  dateRange: 15, 
-					bpmRange: 25, probPick: 100, scoreFilter: 60, sbd_max_graph_distance: music_graph_descriptors.intra_supergenre * 3/2, 
-					dyngenreWeight: 10, dyngenreRange: 2,
-					properties: getPropertiesPairs(this.buttonsProperties, this.prefix, 0)}; // Mix even more varied on styles/genres most from the same decade
-		searchByDistance(args);
-		t1 = Date.now();
-		console.log('Call to searchByDistance SimilarGenres took ' + (t1 - t0) + ' milliseconds.');
+	'Search by Distance similar genres': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Genres', function (mask) {
+		if (mask === MK_SHIFT) {
+			settingsMenu(this, true, ['buttons_search_bydistance.js'], {bAdvTitle: {popup: globRegExp.title.desc}}).btn_up(this.currX, this.currY + this.currH);
+		} else {
+			// Mix even more varied on styles/genres most from the same decade
+			const tags = JSON.parse(this.buttonsProperties.tags[1]);
+			tags.genre.weight = 0; tags.style.weight = 5; tags.mood.weight = 15; tags.key.weight = 10; tags.date.weight = 25; tags.bpm.weight = 5;
+			tags.date.range = 15; tags.bpm.range = 25;
+			tags.dynGenre.weight = 10; tags.dynGenre.range = 2;
+			const defArgs = {tags, bRandomPick: true, bHarmonicMixDoublePass: true, properties: this.buttonsProperties}
+			const args = {...defArgs, minScoreFilter: 55, scoreFilter: 60, graphDistance: music_graph_descriptors.intra_supergenre * 3/2};
+			searchByDistance(args);
+		}
 	}, null, void(0), 'Random mix even more varied on styles/genres, most tracks within a decade', prefix, newButtonsProperties, chars.wand),
-	'Search by Distance similar mood': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Mood', function () {
-		let t0 = Date.now();
-		let t1 = 0;
-		const args = {genreWeight: 0, styleWeight: 5, moodWeight: 15, keyWeight: 10, dateWeight: 0, bpmWeight: 5, 
-					bpmRange: 25, probPick: 100, scoreFilter: 50, sbd_max_graph_distance: music_graph_descriptors.intra_supergenre * 4, 
-					dyngenreWeight: 5, dyngenreRange: 4,
-					properties: getPropertiesPairs(this.buttonsProperties, this.prefix, 0)}; // Mix with different genres but same mood from any date
-		searchByDistance(args);
-		t1 = Date.now();
-		console.log('Call to searchByDistance SimilarMood took ' + (t1 - t0) + ' milliseconds.');
+	'Search by Distance similar mood': new themedButton({x: 0, y: 0, w: 103, h: 22}, 'Similar Mood', function (mask) {
+		if (mask === MK_SHIFT) {
+			settingsMenu(this, true, ['buttons_search_bydistance.js'], {bAdvTitle: {popup: globRegExp.title.desc}}).btn_up(this.currX, this.currY + this.currH);
+		} else {
+			// Mix with different genres but same mood from any date
+			const tags = JSON.parse(this.buttonsProperties.tags[1]);
+			tags.genre.weight = 0; tags.style.weight = 5; tags.mood.weight = 15; tags.key.weight = 10; tags.date.weight = 0; tags.bpm.weight = 5;
+			tags.date.range = 0; tags.bpm.range = 25;
+			tags.dynGenre.weight = 5; tags.dynGenre.range = 4;
+			const defArgs = {tags, bRandomPick: true, bHarmonicMixDoublePass: true, properties: this.buttonsProperties}
+			const args = {...defArgs, minScoreFilter: 45, scoreFilter: 50, graphDistance: music_graph_descriptors.intra_supergenre * 4};
+			searchByDistance(args);
+		}
 	}, null, void(0), 'Random mix with different genres but same mood from any date', prefix, newButtonsProperties, chars.wand),
 });
