0b02c7c5f3cb60d3a451ed3492e2c1c5e9671911
regorxxx <regorxxx@protonmail.com>
2023-02-02 14:41:06 +0100

    - UI: added icons-only mode for toolbar buttons at the toolbar configuration menu ('Other UI configuration'). Tooltip is adjusted to show the button's name there instead. Handy when creating a compact toolbar and icons are good enough to recognize the tools. - UI: background color mismatch when resizing windows and using custom background colors.

--- buttons/helpers/buttons_merged_menu.js
+++ buttons/helpers/buttons_merged_menu.js
@@ -1,5 +1,5 @@
 ﻿'use strict'
-//01/02/23
+//02/02/23
 
 include('..\\..\\helpers\\menu_xxx.js');
 include('..\\..\\helpers\\helpers_xxx.js');
@@ -268,6 +268,13 @@ function createButtonsMenu(name) {
 			buttonsBar.config.orientation = barProperties.orientation[1]; // buttons_xxx.js
 			window.Reload();
 		}});
+		menu.newEntry({menuName, entryText: 'Icons-only mode', func: () => {
+			barProperties.bIconMode[1] = !barProperties.bIconMode[1];
+			overwriteProperties(barProperties);
+			buttonsBar.config.bIconMode = barProperties.bIconMode[1]; // buttons_xxx.js
+			window.Repaint();
+		}});
+		menu.newCheckMenu(menuName, 'Icons-only mode', void(0), () => {return barProperties.bIconMode[1];});
 	}
 	menu.newEntry({entryText: 'sep'});
 	{
@@ -280,6 +287,10 @@ function createButtonsMenu(name) {
 		}});
 		if (readmeList) {
 			// Add additional readmes
+			readmeList['Tagging requisites'] = 'tags_structure.txt';
+			readmeList['Tags sources'] = 'tags_sources.txt';
+			readmeList['Other tags notes'] = 'tags_notes.txt';
+			readmeList['Global tag remapping'] = 'tags_global_remap.txt';
 			if (readmeList.hasOwnProperty('buttons_search_quicksearch.js')) {readmeList['Dynamic queries'] = 'dynamic_query.txt';}
 			// Process
 			menu.newEntry({menuName: subMenu, entryText: 'sep'});
