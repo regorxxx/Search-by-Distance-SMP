8b7574240e745d71b34b92ac76c04aa6acf74478
regorxxx <regorxxx@protonmail.com>
2023-11-15 16:48:59 +0100

    Bump version.

--- buttons/toolbars/_buttons_toolbar.js
+++ buttons/toolbars/_buttons_toolbar.js
@@ -14,7 +14,7 @@
 */
 
 var bLoadTags = true; // Note this must be added before loading helpers! See buttons_search_by_tags_combinations.js and search_same_by.js
-var version = '3.2.0';
+var version = '3.3.0';
 
 {
 	const dependencies = [
